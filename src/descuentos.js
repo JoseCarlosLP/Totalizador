7b04b9494a957d071b2a844ab2322fb8cfbae890
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-02-20 15:37:49 -0400

    Se implemento la funcionalidad de calcular el descuento para precios mayores a 7000 y menores a 10000

--- src/descuentos.js
+++ src/descuentos.js
@@ -2,6 +2,7 @@ function calcularDescuentos(total){
     if(total<1000) return 0;
     if(total>=1000 && total<3000) return 3;
     if(total>=3000 && total<7000) return 5;
+    if(total>=7000 && total<10000) return 7;
 }
 
 export default calcularDescuentos;
