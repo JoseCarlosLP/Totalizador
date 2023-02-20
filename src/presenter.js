d926651a45a2f6faeb535ccef1ac8f846e2f85e6
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-02-20 10:46:59 -0400

    Se implemento la funcionalidad de seleccionar estado y mostrarlo

--- src/presenter.js
+++ src/presenter.js
@@ -1,14 +1,19 @@
 const cant_items=document.querySelector("#cant_items")
 const precio_item=document.querySelector("#precio_item")
+const estado=document.querySelector("#estado");
 const resultado=document.getElementById("resultado");
 const resultado2=document.getElementById("resultado2");
+const resultado3=document.getElementById("resultado3");
 const form1=document.querySelector("#IngresarItems-form");
 const form2=document.querySelector("#IngresarPrecio-form");
+const form3=document.querySelector("#SeleccionarEstado-form");
+
 
 
 form1.addEventListener("submit",(event)=>{
     event.preventDefault();
-    mostrarCantItems();
+    if(cant_items.value!="")
+        mostrarCantItems();
     //resultado.style.display="none";
     //resultado.style.display="block";
 
@@ -16,7 +21,17 @@ form1.addEventListener("submit",(event)=>{
 
 form2.addEventListener("submit",(event)=>{
     event.preventDefault();
-    mostrarPrecioXItem();
+    if(precio_item.value!="")
+        mostrarPrecioXItem();
+    //resultado2.style.display="none";
+    //resultado2.style.display="block";
+
+})
+
+form3.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    if(estado.value!="-")
+        mostrarEstado();
     //resultado2.style.display="none";
     //resultado2.style.display="block";
 
@@ -30,4 +45,8 @@ function mostrarCantItems()
 
 function mostrarPrecioXItem(){
     resultado2.innerHTML="El precio por item es : <b>" + precio_item.value+"</b>";
+}
+
+function mostrarEstado(){
+    resultado3.innerHTML="El estado seleccionado es : <b>"+estado.value+"</b>";
 }
