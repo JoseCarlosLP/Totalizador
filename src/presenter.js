7433f89d8cb6a5a7ec61045e41d0343e3dbe41ab
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-02-20 10:35:18 -0400

    Ingresa el precio por item y lo muestra en pantalla

--- src/presenter.js
+++ src/presenter.js
@@ -1,19 +1,33 @@
 const cant_items=document.querySelector("#cant_items")
+const precio_item=document.querySelector("#precio_item")
 const resultado=document.getElementById("resultado");
-const form=document.querySelector("#totalizador-form");
+const resultado2=document.getElementById("resultado2");
+const form1=document.querySelector("#IngresarItems-form");
+const form2=document.querySelector("#IngresarPrecio-form");
 
-form.addEventListener("submit",(event)=>{
+
+form1.addEventListener("submit",(event)=>{
     event.preventDefault();
-    mostrarNumero();
+    mostrarCantItems();
     //resultado.style.display="none";
-    //ffresultado.style.display="block";
+    //resultado.style.display="block";
 
 })
 
-function mostrarNumero()
+form2.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    mostrarPrecioXItem();
+    //resultado2.style.display="none";
+    //resultado2.style.display="block";
+
+})
+
+function mostrarCantItems()
 {
     ////document.getElementById("cantidad").innerHTML=cant_items.value;
     document.getElementById("resultado").innerHTML="La cantidad de items ingresados es : <b>" + cant_items.value + "</b>";
-
 }
 
+function mostrarPrecioXItem(){
+    resultado2.innerHTML="El precio por item es : <b>" + precio_item.value+"</b>";
+}
